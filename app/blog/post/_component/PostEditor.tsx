5990dfb4cd3733f998eb6d0f2e4cd93929fbefdc
jiyoon0605 <ljy10499@gmail.com>
2024-03-22 18:25:07 +0900

    tinyMce Editor 적용

---
+++ app/blog/post/_component/PostEditor.tsx
@@ -0,0 +1,26 @@
+'use client'
+import React from "react";
+import {Editor} from "@tinymce/tinymce-react";
+
+export default function PostEditor() {
+    const plugins = 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown'
+    const toolbar = 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat';
+    return (
+        <Editor
+            id={'tinyEditor'}
+            apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY}
+            init={{
+                plugins,
+                toolbar,
+                tinycomments_mode: 'embedded',
+                tinycomments_author: 'Author name',
+                mergetags_list: [
+                    { value: 'First.Name', title: 'First Name' },
+                    { value: 'Email', title: 'Email' },
+                ],
+                ai_request: (request: any, respondWith: any) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+            }}
+            initialValue="Welcome to TinyMCE!"
+        />
+    );
+}
---
+++ app/blog/post/new/page.tsx
@@ -0,0 +1,7 @@
+import PostEditor from "@/app/blog/post/_component/PostEditor";
+
+export default function NewPost() {
+    return <div>
+        <PostEditor/>
+    </div>
+}
