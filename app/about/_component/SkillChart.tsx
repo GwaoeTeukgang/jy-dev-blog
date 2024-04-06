c157e69550ae83bc9bdd5fc01c3abc48c318914c
jiyoon0605 <ljy10499@gmail.com>
2024-04-07 02:21:34 +0900

    빌드오류 수정
    - new Image 가 원인 Image는 브라우저의 객체이기 때문에 window 가 있는지 체크하고 사용하게 수정했다.

--- app/about/_component/SkillChart.tsx
+++ app/about/_component/SkillChart.tsx
@@ -1,152 +1,155 @@
 'use client';
 
-import { Bubble } from 'react-chartjs-2';
+import {Bubble} from 'react-chartjs-2';
 import React from 'react';
-import { Chart as ChartJS, LinearScale, PointElement, Tooltip } from 'chart.js';
-import { useMediaQuery } from '@/hooks/useMediaQuery';
+import {Chart as ChartJS, LinearScale, PointElement, Tooltip} from 'chart.js';
+import {useMediaQuery} from '@/hooks/useMediaQuery';
 
 ChartJS.register(LinearScale, PointElement, Tooltip);
 
 const customCanvasBackgroundColor = {
-  id: 'customCanvasBackgroundColor',
-  beforeDraw: (chart: any) => {
-    const { ctx } = chart;
-    ctx.save();
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.fillStyle = '#ffffff';
-    ctx.fillRect(0, 0, chart.width, chart.height);
-    ctx.restore();
-  },
+    id: 'customCanvasBackgroundColor',
+    beforeDraw: (chart: any) => {
+        const {ctx} = chart;
+        ctx.save();
+        ctx.globalCompositeOperation = 'destination-over';
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(0, 0, chart.width, chart.height);
+        ctx.restore();
+    },
 };
 
 export default function SkillChart() {
-  const xMap: number[] = [];
-  const isMobile = useMediaQuery('(max-width: 768px)');
-  const getData = (
-    label: string,
-    level: number,
-    imgSrc: string,
-    size: number = 15,
-  ) => {
-    const icon = new Image();
-    icon.src = imgSrc;
-    icon.width = isMobile ? size / 4 : size;
-    icon.height = isMobile ? size / 4 : size;
+    const xMap: number[] = [];
+    const isMobile = useMediaQuery('(max-width: 768px)');
+    const getData = (
+        label: string,
+        level: number,
+        imgSrc: string,
+        size: number = 15,
+    ) => {
+        if (typeof window !== 'undefined') {
+            const icon = new Image();
+            icon.src = imgSrc;
+            icon.width = isMobile ? size / 4 : size;
+            icon.height = isMobile ? size / 4 : size;
 
-    let x = Math.floor(Math.random() * 20) * 5 + 5;
+            let x = Math.floor(Math.random() * 20) * 5 + 5;
 
-    while (!xMap.every((it) => it !== Math.floor(x))) {
-      console.log(
-        xMap.every((it) => it !== Math.floor(x)),
-        x,
-      );
-      x = Math.floor(Math.random() * 20) * 5 + 5;
-    }
+            while (!xMap.every((it) => it !== Math.floor(x))) {
+                console.log(
+                    xMap.every((it) => it !== Math.floor(x)),
+                    x,
+                );
+                x = Math.floor(Math.random() * 20) * 5 + 5;
+            }
 
-    xMap.push(Math.floor(x));
+            xMap.push(Math.floor(x));
 
-    return {
-      label,
-      data: [{ x, y: level, r: 40 }],
-      pointStyle: icon,
-      pointRadius: 40,
+            return {
+                label,
+                data: [{x, y: level, r: 40}],
+                pointStyle: icon,
+                pointRadius: 40,
+            };
+        }
+        return {}
     };
-  };
 
-  const data = {
-    datasets: [
-      { data: [{ x: 0, y: 0, r: 0 }] },
-      { data: [{ x: 25, y: 3, r: 0 }] },
+    const data = {
+        datasets: [
+            {data: [{x: 0, y: 0, r: 0}]},
+            {data: [{x: 25, y: 3, r: 0}]},
 
-      getData('Oracle DB', 0.3, '/icon/oracle-icon.svg', 40),
-      getData('Git', 2.3, '/icon/git-icon.svg', 55),
-      getData('GitHib', 2, '/icon/github-icon.svg', 55),
-      getData('GitLab', 2.5, '/icon/gitlab-icon.svg', 55),
-      getData('maira DB', 1, '/icon/mariadb-icon.svg', 60),
-      getData('vue.js', 1.7, '/icon/vue-icon.svg', 70),
-      getData('Next.js', 2.5, '/icon/nextjs-icon.svg', 80),
-      getData('spring', 2, '/icon/spring-icon.svg', 80),
-      getData('java', 2, '/icon/java-icon.svg', 90),
-      getData('typescript', 2.2, '/icon/ts-icon.png', 110),
-      getData('React.js', 2.5, '/icon/react-icon.svg', 110),
-      getData('javascript', 2.5, '/icon/js-icon.png', 130),
-    ],
-  };
+            getData('Oracle DB', 0.3, '/icon/oracle-icon.svg', 40),
+            getData('Git', 2.3, '/icon/git-icon.svg', 55),
+            getData('GitHib', 2, '/icon/github-icon.svg', 55),
+            getData('GitLab', 2.5, '/icon/gitlab-icon.svg', 55),
+            getData('maira DB', 1, '/icon/mariadb-icon.svg', 60),
+            getData('vue.js', 1.7, '/icon/vue-icon.svg', 70),
+            getData('Next.js', 2.5, '/icon/nextjs-icon.svg', 80),
+            getData('spring', 2, '/icon/spring-icon.svg', 80),
+            getData('java', 2, '/icon/java-icon.svg', 90),
+            getData('typescript', 2.2, '/icon/ts-icon.png', 110),
+            getData('React.js', 2.5, '/icon/react-icon.svg', 110),
+            getData('javascript', 2.5, '/icon/js-icon.png', 130),
+        ],
+    } as any;
 
-  const options = {
-    scales: {
-      y: {
-        grid: {
-          // display: false
+    const options = {
+        scales: {
+            y: {
+                grid: {
+                    // display: false
+                },
+                beginAtZero: true,
+                ticks: {
+                    min: 1,
+                    max: 10,
+                    stepSize: 0.5,
+                    callback: (value: any, index: any, values: any) => {
+                        switch (value) {
+                            case 0.5:
+                                return 'Experience  ';
+                            case 1.5:
+                                return 'Knowledge  ';
+                            case 2.5:
+                                return 'Strong  ';
+                            default:
+                                return '';
+                        }
+                    },
+                    font: {
+                        family: 'LeferiPoint-BlackObliqueA',
+                        size: isMobile ? 10 : 16,
+                        weight: 'bold',
+                        color: 'black',
+                    },
+                },
+            },
+            x: {
+                display: false,
+            },
         },
-        beginAtZero: true,
-        ticks: {
-          min: 1,
-          max: 10,
-          stepSize: 0.5,
-          callback: (value: any, index: any, values: any) => {
-            switch (value) {
-              case 0.5:
-                return 'Experience  ';
-              case 1.5:
-                return 'Knowledge  ';
-              case 2.5:
-                return 'Strong  ';
-              default:
-                return '';
-            }
-          },
-          font: {
-            family: 'LeferiPoint-BlackObliqueA',
-            size: isMobile ? 10 : 16,
-            weight: 'bold',
-            color: 'black',
-          },
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: (context: any) => {
+                        if (
+                            context.datasetIndex !== undefined &&
+                            context.dataIndex !== undefined
+                        ) {
+                            const datasetLabel =
+                                context.chart.data.datasets[context.datasetIndex].label || '';
+                            return datasetLabel;
+                        }
+                        return '';
+                    },
+                },
+                backgroundColor: '#FFF',
+                bodyColor: '#000',
+                bodyFont: {
+                    family: 'LeferiPoint-BlackObliqueA',
+                    size: 15,
+                    color: 'black',
+                },
+                padding: 10,
+                xAlign: 'center',
+                yAlign: 'bottom',
+                borderColor: '#fc5f6a',
+                borderWidth: 1,
+                displayColors: false,
+            },
         },
-      },
-      x: {
-        display: false,
-      },
-    },
-    plugins: {
-      tooltip: {
-        callbacks: {
-          label: (context: any) => {
-            if (
-              context.datasetIndex !== undefined &&
-              context.dataIndex !== undefined
-            ) {
-              const datasetLabel =
-                context.chart.data.datasets[context.datasetIndex].label || '';
-              return datasetLabel;
-            }
-            return '';
-          },
-        },
-        backgroundColor: '#FFF',
-        bodyColor: '#000',
-        bodyFont: {
-          family: 'LeferiPoint-BlackObliqueA',
-          size: 15,
-          color: 'black',
-        },
-        padding: 10,
-        xAlign: 'center',
-        yAlign: 'bottom',
-        borderColor: '#fc5f6a',
-        borderWidth: 1,
-        displayColors: false,
-      },
-    },
-  } as any;
+    } as any;
 
-  return (
-    <div className={'p-1 lg:m-10 mb-10 bg-white rounded-lg'}>
-      <Bubble
-        options={options}
-        plugins={[customCanvasBackgroundColor]}
-        data={data}
-      />
-    </div>
-  );
+    return (
+        <div className={'p-1 lg:m-10 mb-10 bg-white rounded-lg'}>
+            <Bubble
+                options={options}
+                plugins={[customCanvasBackgroundColor]}
+                data={data}
+            />
+        </div>
+    );
 }
--- app/blog/_component/PostList.tsx
+++ app/blog/_component/PostList.tsx
@@ -1,113 +1,111 @@
 'use client';
 
 import PostItem from '@/app/blog/_component/PostItem';
-import { tv } from 'tailwind-variants';
-import { PostItemInfo } from '@/model/blog';
-import { useEffect, useRef, useState } from 'react';
+import {tv} from 'tailwind-variants';
+import {PostItemInfo} from '@/model/blog';
+import {useEffect, useRef, useState} from 'react';
 import useBookmarkStore from '@/store/useBookmarkStore';
-import { AnimatePresence, motion } from 'framer-motion';
-import { MetaPagination } from '@/model';
+import {AnimatePresence, motion} from 'framer-motion';
+import {MetaPagination} from '@/model';
 import usePaginationInfo from '@/hooks/usePaginationInfo';
-import { getPaginatedPost } from '@/lib/api/blog';
+import {getPaginatedPost} from '@/lib/api/blog';
+
+const callMorePost = async (setPagination: (data: PostItemInfo[], meta: MetaPagination) => void, page: number) => {
+    try {
+        const {data} = await getPaginatedPost(page, 10);
+        return setPagination(data.data, data.meta);
+    } catch (e) {
+        console.error(e);
+    }
+};
 
 const postListContainer = tv({
-  base: [
-    'grid',
-    'grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))]',
-    'gap-4',
-    'p-10',
-    'max-sm:p-2',
-    'mb-4',
-    'max-h-[1200px]',
-    'overflow-y-scroll',
-  ],
+    base: [
+        'grid',
+        'grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))]',
+        'gap-4',
+        'p-10',
+        'max-sm:p-2',
+        'mb-4',
+        'max-h-[1200px]',
+        'overflow-y-scroll',
+    ],
 });
 
 export default function PostList({
-  postList,
-  meta,
-}: {
-  postList: PostItemInfo[];
-  meta: MetaPagination;
+                                     postList,
+                                     meta,
+                                 }: {
+    postList: PostItemInfo[];
+    meta: MetaPagination;
 }) {
-  const { initBookmarks } = useBookmarkStore();
-  const targetElement = useRef<HTMLDivElement>(null);
-  const { list, pageInfo, setPagination, hasMorePost } =
-    usePaginationInfo<PostItemInfo>(postList, meta);
+    const {initBookmarks} = useBookmarkStore();
+    const targetElement = useRef<HTMLDivElement>(null);
+    const {list, pageInfo, setPagination, hasMorePost} =
+        usePaginationInfo<PostItemInfo>(postList, meta);
 
-  useEffect(() => {
-    const bookmarks = (localStorage.getItem('bookmarks') ?? '').split(',');
-    initBookmarks(bookmarks ?? []);
-  }, [initBookmarks]);
+    useEffect(() => {
+        const bookmarks = (localStorage.getItem('bookmarks') ?? '').split(',');
+        initBookmarks(bookmarks ?? []);
+    }, [initBookmarks]);
 
-  const callMorePost = async () => {
-    if (!hasMorePost) {
-      return;
-    }
-    try {
-      const { data } = await getPaginatedPost(pageInfo.page + 1, 10);
-      setPagination(data.data, data.meta);
-    } catch (e) {
-      console.error(e);
-    }
-  };
 
-  useEffect(() => {
-    const observeCallBack = (
-      entries: IntersectionObserverEntry[],
-      observer: IntersectionObserver,
-    ) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          callMorePost();
+    useEffect(() => {
+        const observeCallBack = (
+            entries: IntersectionObserverEntry[],
+            observer: IntersectionObserver,
+        ) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    callMorePost(setPagination, pageInfo.page + 1);
+                }
+            });
+        };
+        const observer = new IntersectionObserver(observeCallBack, {
+            root: null,
+            threshold: 0.1,
+        });
+
+        const target = targetElement.current;
+
+        if (target) {
+            observer.observe(target);
         }
-      });
-    };
-    const observer = new IntersectionObserver(observeCallBack, {
-      root: null,
-      threshold: 0.1,
-    });
 
-    const target = targetElement.current;
+        return () => {
+            if (target) {
+                observer.unobserve(target);
+            }
+        };
+    }, [list, pageInfo, setPagination, hasMorePost]);
 
-    if (target) {
-      observer.observe(target);
-    }
-
-    return () => {
-      if (target) {
-        observer.unobserve(target);
-      }
-    };
-  }, [list, pageInfo, setPagination, hasMorePost, callMorePost]);
-
-  return (
-    <AnimatePresence mode={'wait'}>
-      <ul className={postListContainer()}>
-        {list.map((item, index) => (
-          <motion.li
-            key={item.id}
-            initial={{ ...animate(index).initial }}
-            animate={animate(index).animate}
-          >
-            <PostItem {...item} />
-          </motion.li>
-        ))}
-      </ul>
-      <div className={'w-full h-2 bg-transparent'} ref={targetElement} />
-    </AnimatePresence>
-  );
+    return (
+        <AnimatePresence mode={'wait'}>
+            <ul className={postListContainer()}>
+                {list.map((item, index) => (
+                    <motion.li
+                        key={item.id}
+                        initial={{...animate(index).initial}}
+                        animate={animate(index).animate}
+                    >
+                        <PostItem {...item} />
+                    </motion.li>
+                ))}
+            </ul>
+            <div className={'w-full h-2 bg-transparent'} ref={targetElement}/>
+        </AnimatePresence>
+    );
 }
 
 const animate = (delay: number) => ({
-  initial: {
-    translateY: 50,
-    opacity: 0,
-    transition: { duration: 0.3 },
-  },
-  animate: {
-    translateY: 0,
-    opacity: 1,
-    transition: { duration: 0.3, delay: (0.2 * delay) % 10 },
-  },
+    initial: {
+        translateY: 50,
+        opacity: 0,
+        transition: {duration: 0.3},
+    },
+    animate: {
+        translateY: 0,
+        opacity: 1,
+        transition: {duration: 0.3, delay: (0.2 * delay) % 10},
+    },
 });
