b93d379e6a80fd277048c1c32855bad15a278516
jiyoon0605 <ljy10499@gmail.com>
2024-04-07 04:22:08 +0900

    배포환경에서 three.js 3d 모델 렌더링 안되는 문제 수정
    - 에러 위치 찾기 테스트

--- app/_component/scene/Scene.tsx
+++ app/_component/scene/Scene.tsx
@@ -47,34 +47,34 @@ const Scene = () => {
   const [cameraPos, setCameraPos] = useState<Vector3>(new Vector3(-1, 1.5, -2));
 
   useEffect(() => {
-      if (isMobile) {
-        setHeight('90vh');
-        setCameraPos(new Vector3(-2, 9, -2));
-      }
-      const targetNode = document.querySelector('html');
-      if (!targetNode) {
-        return;
-      }
+    if (isMobile) {
+      setHeight('90vh');
+      setCameraPos(new Vector3(-2, 9, -2));
+    }
+    const targetNode = document.querySelector('html');
+    if (!targetNode) {
+      return;
+    }
 
-      setIsDarkMode(targetNode.classList.contains('dark'));
+    setIsDarkMode(targetNode.classList.contains('dark'));
 
-      const observer = new MutationObserver((mutationsList, observer) => {
-        mutationsList.forEach((mutation) => {
-          if (
-            mutation.type === 'attributes' &&
-            mutation.attributeName === 'class'
-          ) {
-            setIsDarkMode(targetNode.classList.contains('dark'));
-          }
-        });
+    const observer = new MutationObserver((mutationsList, observer) => {
+      mutationsList.forEach((mutation) => {
+        if (
+          mutation.type === 'attributes' &&
+          mutation.attributeName === 'class'
+        ) {
+          setIsDarkMode(targetNode.classList.contains('dark'));
+        }
       });
+    });
 
-      observer.observe(targetNode, {
-        attributes: true,
-        attributeOldValue: true,
-        attributeFilter: ['class'],
-      });
-      return () => observer.disconnect();
+    observer.observe(targetNode, {
+      attributes: true,
+      attributeOldValue: true,
+      attributeFilter: ['class'],
+    });
+    return () => observer.disconnect();
   }, [isMobile]);
 
   return (
@@ -84,35 +84,39 @@ const Scene = () => {
       <OrbitControls />
       <ambientLight intensity={1} />
       <directionalLight position={[0, 10, 0]} intensity={1.5} />
-      {/*<Text*/}
-      {/*  position={[8, 1, 2]}*/}
-      {/*  rotation={[0.3, 10, 0.1]}*/}
-      {/*  fontSize={0.8}*/}
-      {/*  color={isDarkMode ? 'white' : 'black'}*/}
-      {/*  anchorX="left"*/}
-      {/*  anchorY="middle"*/}
-      {/*  maxWidth={10}*/}
-      {/*  font={*/}
-      {/*    'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiPoint-BlackObliqueA.woff'*/}
-      {/*  }*/}
-      {/*>*/}
-      {/*  Hello World!*/}
-      {/*</Text>*/}
-      {/*<Text*/}
-      {/*  position={[8.2, -0.1, 2]}*/}
-      {/*  rotation={[0.3, 10, 0.1]}*/}
-      {/*  fontSize={0.4}*/}
-      {/*  color={isDarkMode ? 'white' : 'black'}*/}
-      {/*  anchorX="left"*/}
-      {/*  anchorY="middle"*/}
-      {/*  maxWidth={10}*/}
-      {/*  font={*/}
-      {/*    'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiPoint-WhiteObliqueA.woff'*/}
-      {/*  }*/}
-      {/*>*/}
-      {/*  Welcome to my developer blog and portfolio, where I share insights and*/}
-      {/*  projects.*/}
-      {/*</Text>*/}
+      {typeof window !== 'undefined' && (
+        <>
+          <Text
+            position={[8, 1, 2]}
+            rotation={[0.3, 10, 0.1]}
+            fontSize={0.8}
+            color={isDarkMode ? 'white' : 'black'}
+            anchorX="left"
+            anchorY="middle"
+            maxWidth={10}
+            font={
+              'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiPoint-BlackObliqueA.woff'
+            }
+          >
+            Hello World!
+          </Text>
+          <Text
+            position={[8.2, -0.1, 2]}
+            rotation={[0.3, 10, 0.1]}
+            fontSize={0.4}
+            color={isDarkMode ? 'white' : 'black'}
+            anchorX="left"
+            anchorY="middle"
+            maxWidth={10}
+            font={
+              'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiPoint-WhiteObliqueA.woff'
+            }
+          >
+            Welcome to my developer blog and portfolio, where I share insights
+            and projects.
+          </Text>
+        </>
+      )}
     </Canvas>
   );
 };
