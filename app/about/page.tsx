e4cd2335ba363d7bc2851a55d2a4bdb9065be6fb
jiyoon0605 <ljy10499@gmail.com>
2024-04-29 22:06:25 +0900

    [FIX] about 문구 수정

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -22,7 +22,7 @@ export default function Profile() {
           <div>
             <h2 className={'text-2xl font-bold'}>이지윤</h2>
             <h2 className={'text-2xl font-bold'}>Lee Ji yoon</h2>
-            <p className={'mt-1 text-lg text-gray-400'}>Web Developer</p>
+            <p className={'mt-1 text-lg text-gray-400'}>Web Frontend Developer</p>
             <div className={'my-4 text-gray-400'}>
               <div>2003.06.05</div>
               <Link
@@ -47,10 +47,9 @@ export default function Profile() {
                 {GITHUB}
               </Link>
             </div>
-            <p className={'mt-4'}>
+            <p className={'mt-4  max-w-1/2'}>
               3년차 웹 프론트엔드 개발자 이지윤입니다.
-              <br /> 현재는 웹 프론트엔드에 더 많은 관심을 가지고 있습니다.
-              사용자 중심의 디자인과 사용자 경험을 중요하게 생각하며, 최신
+              <br /> 사용자 중심의 디자인과 사용자 경험을 중요하게 생각하며, 최신
               기술을 통해 안정적이고 효율적인 서비스를 제공하고자 노력합니다.
               견고한 기반 위에 차곡차곡 쌓아 올린 경험과 지식을 바탕으로
               프로젝트를 진행하고 있습니다.
